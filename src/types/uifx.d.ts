2073f20367f68288107f0ae0893f4de895e78d71
mrhitman <x-_man@mail.ru>
2019-08-05 22:06:32 +0300

    add firebase

--- src/types/uifx.d.ts
+++ src/types/uifx.d.ts
@@ -2,11 +2,12 @@ declare module 'uifx' {
   interface Config {
     asset: string;
     throttleMs?: number;
+    volume?: number;
   }
 
   declare class UIFx {
     constructor (config: Config);
-    play(): void;
+    play(volume?: number): void;
     setVolume(value: number): void;
   }
 
