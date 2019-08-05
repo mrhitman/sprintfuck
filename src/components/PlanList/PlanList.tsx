b51ca31763da5006744bf00384a9e74b461e107c
mrhitman <x-_man@mail.ru>
2019-08-05 20:52:49 +0300

    todo

--- src/components/PlanList/PlanList.tsx
+++ src/components/PlanList/PlanList.tsx
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
-import { Col, FormControl, InputGroup, Row } from 'react-bootstrap';
+import { Button, Col, Container, FormControl, InputGroup, Row } from 'react-bootstrap';
+import { FaStar } from 'react-icons/fa';
 
 export class PlanList extends Component {
   public render() {
     return (
-      <React.Fragment>
+      <Container className="planlist-panel">
         <Row>
-          <Col>
+          <Col xs={5}>
             <InputGroup>
               <FormControl />
             </InputGroup>
           </Col>
-          <Col>
+          <Col xs={6}>
             <InputGroup>
               <FormControl />
+              <InputGroup.Append>
+                <InputGroup.Text>+</InputGroup.Text>
+              </InputGroup.Append>
             </InputGroup>
+          </Col>
+          <Col xs={1}>
+            <Button variant="outline-warning">
+              <FaStar size="13" />
+            </Button>
           </Col>
         </Row>
-      </React.Fragment>
+      </Container>
     );
   }
 }
