5405e3db5bbceffdddf8f53e69f1114f5ab5cf45
hitman <hitman@iMac-hitman.Office.WebDevs.US>
2019-08-08 18:16:37 +0300

    add plan list item

--- src/components/PlanList/PlanList.tsx
+++ src/components/PlanList/PlanList.tsx
@@ -1,18 +1,48 @@
-import React, { Component } from 'react';
-import { Button, Col, Container, FormControl, InputGroup, Row } from 'react-bootstrap';
-import { FaStar } from 'react-icons/fa';
+import { DateTime } from "luxon";
+import React, { Component } from "react";
+import { Button, Col, Container, FormControl, InputGroup, ListGroup, Row } from "react-bootstrap";
+import { FaEllipsisH, FaStar } from "react-icons/fa";
+import uuid from "uuid";
 
-export class PlanList extends Component {
+interface PlanItem {
+  id: string;
+  category: string;
+  description: string;
+  order: number;
+  amount: number;
+  is_favorite: boolean;
+  created_at: DateTime;
+}
+
+interface PlanListState {
+  items: PlanItem[];
+}
+
+export class PlanList extends Component<{}, PlanListState> {
+  public state = {
+    items: [
+      {
+        id: uuid(),
+        category: "work",
+        description: "do pomidoro",
+        order: 1,
+        is_favorite: false,
+        amount: 1,
+        created_at: DateTime.local()
+      }
+    ]
+  };
+
   public render() {
     return (
       <Container className="planlist-panel">
         <Row>
-          <Col xs={5}>
+          <Col xs={3}>
             <InputGroup>
               <FormControl />
             </InputGroup>
           </Col>
-          <Col xs={6}>
+          <Col xs={8}>
             <InputGroup>
               <FormControl />
               <InputGroup.Append>
@@ -24,6 +54,29 @@ export class PlanList extends Component {
             <Button variant="outline-warning">
               <FaStar size="13" />
             </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ListGroup variant="flush">
+              {this.state.items.map(item => (
+                <ListGroup.Item className="planlist-item" style={{ borderBottom: '1px solid rgba(75%, 75%, 75%, 1)' }}>
+                  <Row>
+                    <Col xs={3}>{item.category}</Col>
+                    <Col xs={6}>{item.description}</Col>
+                    <Col xs={1}>{item.created_at.toFormat("HH:mm")}</Col>
+                    <Col xs={1}>
+                      <Button variant="outline-info">{item.amount}</Button>
+                    </Col>
+                    <Col xs={1}>
+                      <Button variant="outline-info">
+                        <FaEllipsisH size="13" />
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
           </Col>
         </Row>
       </Container>
