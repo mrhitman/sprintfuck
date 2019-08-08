af8246e6b8c357a7e452dad5a67a8d34cdbe7fc2
mrhitman <x-_man@mail.ru>
2019-08-08 23:35:43 +0300

    order items

--- src/components/PlanList/PlanList.tsx
+++ src/components/PlanList/PlanList.tsx
@@ -1,18 +1,17 @@
-import { DateTime } from 'luxon';
-import React, { Component, createRef } from 'react';
-import { Button, Col, Container, FormControl, InputGroup, ListGroup, Row } from 'react-bootstrap';
-import { FaStar } from 'react-icons/fa';
+import {DateTime} from 'luxon';
+import React, {Component, createRef} from 'react';
+import {Button, Col, Container, FormControl, InputGroup, ListGroup, Row} from 'react-bootstrap';
+import {FaStar} from 'react-icons/fa';
 import uuid from 'uuid';
-import PlanListItem, { PlanItem } from './PlanListItem';
-import Form from 'react-bootstrap/FormGroup';
+import PlanListItem, {PlanItem} from './PlanListItem';
 
 interface PlanListState {
   items: PlanItem[];
 }
 
 export class PlanList extends Component<{}, PlanListState> {
-  protected categoryRef = createRef<any>()
-  protected descriptionRef  = createRef<any>();
+  protected categoryRef = createRef<any>();
+  protected descriptionRef = createRef<any>();
   public state = {
     items: [
       {
@@ -33,7 +32,7 @@ export class PlanList extends Component<{}, PlanListState> {
         <Row>
           <Col xs={3}>
             <InputGroup>
-              <FormControl ref={this.categoryRef} />
+              <FormControl ref={this.categoryRef}/>
             </InputGroup>
           </Col>
           <Col xs={8}>
@@ -46,7 +45,7 @@ export class PlanList extends Component<{}, PlanListState> {
           </Col>
           <Col xs={1}>
             <Button variant="outline-warning">
-              <FaStar size="13" />
+              <FaStar size="13"/>
             </Button>
           </Col>
         </Row>
@@ -54,7 +53,12 @@ export class PlanList extends Component<{}, PlanListState> {
           <Col>
             {this.state.items.length ? (
               <ListGroup variant="flush">
-                {this.state.items.map((item) => <PlanListItem key={item.id} {...item} />)}
+                {this.state
+                  .items
+                  .sort()
+                  .reverse()
+                  .map((item) => <PlanListItem
+                    key={item.id} {...item} />)}
               </ListGroup>
             ) : (
               <div>There no any items</div>
@@ -65,14 +69,28 @@ export class PlanList extends Component<{}, PlanListState> {
     );
   }
 
-  protected handleEnter = (e: React.SyntheticEvent<HTMLInputElement>) => {
-    this.addItem({
-      category: this.categoryRef.current.value,
-      description: this.descriptionRef.current.value
-    });
+  protected handleEnter = (e: any) => {
+    if (e.key === 'Enter') {
+      this.addItem({
+        category: this.categoryRef.current.value,
+        description: this.descriptionRef.current.value
+      });
+    }
   }
 
+  protected isEqualItem = (a: any, b: any) => {
+      return a.category === b.category && a.description === b.description
+  };
+
   protected addItem = (values: Partial<PlanItem>) => {
+    const items = this.state.items;
+    const lastItem = items.reduce((acc, item) => item.order > acc.order ? item : acc);
+    if (this.isEqualItem(values, lastItem)) {
+      const index = items.indexOf(lastItem);
+      items.splice(index, 1);
+      this.setState({items: [...items, {...lastItem, amount: lastItem.amount + 1}]});
+      return;
+    }
     this.setState((state) => ({
       items: [
         ...state.items,
